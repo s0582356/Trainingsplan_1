80d91aceba8d97801ca11f9416566db811e72c44
Altan <s0582356>
2024-10-21 03:15:59 +0200

    Das Projekt läuft durch! Commit 1

---
+++ src/main/java/de/Webtechnologien/Trainingplan1Application.java
@@ -0,0 +1,13 @@
+package de.Webtechnologien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trainingplan1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trainingplan1Application.class, args);
+	}
+
+}
---
+++ src/test/java/de/Webtechnologien/Trainingplan1ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.Webtechnologien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Trainingplan1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
